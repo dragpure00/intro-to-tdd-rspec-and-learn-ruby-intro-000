7f255df268e13fdc22662f24ed932f344286ea26
Ayaz Araf <ayaz.araf@gmail.com>
2020-03-16 18:22:53 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,6 @@
 def current_age_for_birth_year(birth_year)
   current_year = 2003;
-puts current_age = current_year - birth_year
+ current_age = current_year - birth_year
   end
 
   current_age_for_birth_year(1984)
